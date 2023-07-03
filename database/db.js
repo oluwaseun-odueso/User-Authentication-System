ae8fbc17d64fc15b9f83c462d6cb102b76cb001f
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-07-03 10:44:49 +0100

    Database connection

---
+++ database/db.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const database = process.env.SQ_DATABASE;
+const username = process.env.SQ_USERNAME;
+const password = process.env.SQ_PASSWORD;
+const host = process.env.SQ_HOST;
+
+if (!database || !user || !password) {
+  throw new Error(
+    "Missing required environment variables for database connection"
+  );
+}
+
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: "mysql",
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((error) => console.log("Unable to connect to the database:", error));
+
+export default sequelize;
